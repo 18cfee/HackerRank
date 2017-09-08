4bb48057ae19eb5d20bf344373dfcb6a88cc4d84
Carl Fee <cfee@localhost.localdomain>
2017-09-07 23:16:08 -0600

    master-prep

--- src/Solution.java
+++ src/Solution.java
@@ -1,2 +1,13 @@
+import java.io.*;
+import java.util.*;
+
 public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int M = in.nextInt();
+        int N = in.nextInt();
+        int R = in.nextInt();
+        System.out.println("Master");
+    }
 }
