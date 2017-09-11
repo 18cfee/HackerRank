93c3b74b007c4fed95c741123a42b9bb2baa2289
Carl Fee <cfee@localhost.localdomain>
2017-09-10 21:33:59 -0600

    stiil-need-to-solve

--- src/Solution.java
+++ src/Solution.java
@@ -5,9 +5,27 @@ public class Solution {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int M = in.nextInt();
-        int N = in.nextInt();
-        int R = in.nextInt();
-        System.out.println("Master");
+        int n = in.nextInt();
+        long finalNum = 1;
+        int[] array = new int[n+1];
+        for(int i = 0; i < n - 1; i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            array[a]++;
+            array[b]++;
+        }
+        int pos = 0;
+        for(int i = 1; i <= n; i++){
+            if(array[i] > 1){
+                pos++;
+            }
+        }
+        long mod = 1000000007;
+
+        for(int i = 0; i < pos; i++){
+            finalNum = 2*finalNum%mod;
+        }
+
+        System.out.println(finalNum);
     }
 }
