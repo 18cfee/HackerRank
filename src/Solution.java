2dc995394d174f373a0dde7e76717e29b97d0190
Carl Fee <cfee@localhost.localdomain>
2017-09-17 16:04:52 -0600

    rand

--- src/Solution.java
+++ src/Solution.java
@@ -5,9 +5,8 @@ public class Solution {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int M = in.nextInt();
         int N = in.nextInt();
-        int R = in.nextInt();
+        int[] array =
         System.out.println("Master");
     }
 }
