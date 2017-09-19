307cdd43c10a6d83086c6a31a26ba0099eb4de22
Carl Fee <cfee@localhost.localdomain>
2017-09-19 17:05:39 -0600

    ignore

--- src/Solution.java
+++ src/Solution.java
@@ -6,7 +6,16 @@ public class Solution {
     public static void main(String[] args) throws FileNotFoundException{
         Scanner in = new Scanner(new File("sol.in"));
         //Scanner in = new Scanner(System.in);
-        int N = in.nextInt();
+        int n = in.nextInt();
+        int[] socks = new int[101];
+        for(int i = 0; i < n; i++){
+            socks[in.nextInt()]++;
+        }
+        int tot = 0;
+        for(int i = 0; i < 101; i++){
+            tot += socks[i]/2;
+        }
+        System.out.println(tot);
 
     }
 }
