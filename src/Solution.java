00d39d6dda79239616a2768ee5baed46ed37bed2
Carl Fee <cfee@localhost.localdomain>
2017-09-19 18:23:32 -0600

    counting-valleys-15

--- src/Solution.java
+++ src/Solution.java
@@ -7,9 +7,28 @@ public class Solution {
         Scanner in = new Scanner(new File("sol.in"));
         //Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        int p = in.nextInt();
-        n = n - n%2;
-        int tot = Math.min((p)/2, (n - p + 1)/2);
+        String mount = in.next();
+        int current = 0;
+        int tot = 0;
+        char a = mount.charAt(0);
+        boolean up = false;
+        if(a == 'U') {
+            up = true;
+            current++;
+        }else{
+            tot++;
+            current--;
+        }
+        for(int i = 1; i < n; i++){
+            a = mount.charAt(i);
+            if(a == 'U') current++;
+            else current--;
+            if(current >= 0) up = true;
+            else if(current < 0 && up == true){
+                tot++;
+                up = false;
+            }
+        }
         System.out.println(tot);
     }
 }
