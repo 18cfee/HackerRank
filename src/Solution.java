e6b4fc71189e98b4584b6db29907ff54258d707d
Carl Fee <cfee@localhost.localdomain>
2017-09-19 18:31:17 -0600

    electrnic-shop-15

--- src/Solution.java
+++ src/Solution.java
@@ -3,32 +3,35 @@ import java.util.*;
 
 public class Solution {
 
+    static int getMoneySpent(int[] keyboards, int[] drives, int s){
+        int a = keyboards.length;
+        int b = drives.length;
+        int max =-1;
+        for(int i = 0; i < a; i++){
+            for(int j= 0; j < b; j++){
+                int cur = keyboards[i] + drives[j];
+                if(cur > s) cur = -1;
+                max = Math.max(max,cur);
+            }
+        }
+        return max;
+    }
     public static void main(String[] args) throws FileNotFoundException{
         Scanner in = new Scanner(new File("sol.in"));
         //Scanner in = new Scanner(System.in);
+        int s = in.nextInt();
         int n = in.nextInt();
-        String mount = in.next();
-        int current = 0;
-        int tot = 0;
-        char a = mount.charAt(0);
-        boolean up = false;
-        if(a == 'U') {
-            up = true;
-            current++;
-        }else{
-            tot++;
-            current--;
+        int m = in.nextInt();
+        int[] keyboards = new int[n];
+        for(int keyboards_i=0; keyboards_i < n; keyboards_i++){
+            keyboards[keyboards_i] = in.nextInt();
         }
-        for(int i = 1; i < n; i++){
-            a = mount.charAt(i);
-            if(a == 'U') current++;
-            else current--;
-            if(current >= 0) up = true;
-            else if(current < 0 && up == true){
-                tot++;
-                up = false;
-            }
+        int[] drives = new int[m];
+        for(int drives_i=0; drives_i < m; drives_i++){
+            drives[drives_i] = in.nextInt();
         }
-        System.out.println(tot);
+        //  The maximum amount of money she can spend on a keyboard and USB drive, or -1 if she can't purchase both items
+        int moneySpent = getMoneySpent(keyboards, drives, s);
+        System.out.println(moneySpent);
     }
 }
