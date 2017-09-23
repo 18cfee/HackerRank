88b11b747c2b5509f01c125bf483541adecd59d6
Carl Fee <cfee@localhost.localdomain>
2017-09-22 23:24:44 -0600

    grading-students-10

--- src/Solution.java
+++ src/Solution.java
@@ -6,37 +6,10 @@ public class Solution {
         Scanner in = new Scanner(new File("sol.in"));
         //Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        List<Integer> scores = new ArrayList<Integer>(n);
-        int currentS = in.nextInt();
-        scores.add(currentS);
-        for(int scores_i=0; scores_i < n - 1; scores_i++){
-            int newS = in.nextInt();
-            if(newS != currentS){
-                scores.add(newS);
-                currentS = newS;
-            }
-        }
-        int m = in.nextInt();
-        int[] alice = new int[m];
-        for(int alice_i=0; alice_i < m; alice_i++){
-            alice[alice_i] = in.nextInt();
-        }
-        int score = 0;
-        int tot = scores.size();
-        int curInd = tot - 1;
-
-        for(int i = 0; i < m; i++){
-            score = alice[i];
-            while(curInd >= 0 && score > scores.get(curInd)){
-                curInd--;
-            }
-            if(curInd == -1) {
-                System.out.println(1);
-            }
-            else if(score >= scores.get(curInd)) System.out.println(curInd + 1);
-            else{
-                System.out.println(curInd + 2);
-            }
+        for(int i = 0; i < n; i++){
+            int next = in.nextInt();
+            if(next > 37 && next%5 > 2) next += (5-next%5);
+            System.out.println(next);
         }
     }
 }
